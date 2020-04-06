35cf8d169011b21429f732bf329d5fe832b7aa75
nick-lrg <62886517+nick-lrg@users.noreply.github.com>
2020-04-06 13:12:59 +0200

    Revert "Improve ExpectedElement exception message"
    This reverts commit 8411a6aa69b1f66db2b1a0b45d9fcb2e33c7d6f0.

--- src/datamodel/ExpectedElement.cpp
+++ src/datamodel/ExpectedElement.cpp
@@ -8,6 +8,5 @@ ExpectedElement::ExpectedElement(std::string name, std::string ns)
 
 const char* ExpectedElement::what() const noexcept
 {
-  auto msg = "expected element " + name_ + " from namespace " + ns_ + " not encountered";
-  return msg.c_str();
+  return "expected element not encountered";
 }
