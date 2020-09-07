ffe0a730782add8cfe4e6d5e54cc8f9107c8e028
Maximilian Schmidt <maximilian@schmidt.so>
2020-09-07 12:17:09 +0200

    Delete all copy/moment/assignment operators from ClientSession

--- ports/esp/ClientSession.esp.hpp
+++ ports/esp/ClientSession.esp.hpp
@@ -8,6 +8,10 @@ class ClientSessionEsp32 : public ClientSessionInterface
 {
 public:
   explicit ClientSessionEsp32(std::string notifyTo);
+  ClientSessionEsp32(const ClientSessionEsp32&) = delete;
+  ClientSessionEsp32(ClientSessionEsp32&&) = delete;
+  ClientSessionEsp32& operator=(const ClientSessionEsp32&) = delete;
+  ClientSessionEsp32& operator=(ClientSessionEsp32&&) = delete;
   ~ClientSessionEsp32() override;
   void send(const std::string& message) override;
 
