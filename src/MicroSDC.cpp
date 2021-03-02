1721d24416322a4a1f0e73bbdad30b84c5f3c800
Maximilian Schmidt <maximilian@schmidt.so>
2021-03-02 12:01:26 +0100

    formatting

--- src/MicroSDC.cpp
+++ src/MicroSDC.cpp
@@ -76,7 +76,8 @@ void MicroSDC::startup()
   }
   if (networkConfig_->discoveryProxy().has_value())
   {
-    LOG(LogLevel::INFO, "Configuring discovery Proxy: " << networkConfig_->discoveryProxy().value());
+    LOG(LogLevel::INFO,
+        "Configuring discovery Proxy: " << networkConfig_->discoveryProxy().value());
     discoveryService_->configureProxy(networkConfig_->discoveryProxyProtocol(),
                                       networkConfig_->discoveryProxy().value());
   }
